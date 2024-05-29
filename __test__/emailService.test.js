0d93f100f8f4a18b68d7abd53f308c2481d70183
jordan toujouse <jtoujo1@gmail.com>
2024-05-29 18:43:36 -0500

    Fixed my Email service test and got it to pass.

--- __test__/emailService.test.js
+++ __test__/emailService.test.js
@@ -1,16 +1,15 @@
-import sendEmail from "../emailService";
+const { sendEmail } = require("../emailService");
 
-jest.mock("./sendEmail", () => ({
+jest.mock("../emailService", () => ({
   sendEmail: jest.fn(() => Promise.resolve("Email sent")),
 }));
 
 describe("Email sending", () => {
   it("should send email to the user", () => {
-    const mockSendEmail = jest.fn();
     const email = "test@example.com";
     const message = "test message";
-    sendEmail.mockImplentation(mockSendEmail);
+
     sendEmail(email, message);
-    expect(mockSendEmail).toHaveBeenCalledWith(email, message);
+    expect(sendEmail).toHaveBeenCalledWith(email, message);
   });
 });
